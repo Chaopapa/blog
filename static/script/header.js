8b24ea5a6f4aa4cb65e87780d28021cb05b5a55e
cc <1321607222@qq.com>
2019-10-03 21:55:00 +0800

    使用session保存导航栏样式

--- static/script/header.js
+++ static/script/header.js
@@ -1,6 +1,14 @@
-var navList = $(".c-header-nav").children();
-console.log(1);
-navList.click(function() {
-  navList.removeClass("current_page");
-  $(this).addClass("current_page");
+$(function() {
+  var navList = $(".c-header-nav").children();
+  //页面载入通过session中的值确定头部哪个导航应该添加样式
+  var index = sessionStorage.getItem("index");
+  index ? navList.eq(index).addClass("current_page") : navList.eq(0).addClass("current_page");
+  console.log(navList.eq(0));
+  navList.click(function() {
+    navList.removeClass("current_page");
+    $(this).addClass("current_page");
+    var listIndex = $(this).attr("data-index");
+    //记录下标
+    sessionStorage.setItem("index", listIndex);
+  });
 });
